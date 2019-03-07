b8138c2daaceca16b896cdcf43d506d83cd457fa
semasemenov2015 <semasemenov2015@yandex.ru>
2019-03-07 12:45:05 +0300

    Новый

--- js33.js
+++ js33.js
@@ -1,3 +1,20 @@
-/**
- * Created by User on 06.03.2019.
- */
+'use strict';
+
+/*3 Нарисовать горку с помощью console.log (используя цикл for), как показано на
+ рисунке,
+ только у вашей горки должно быть 20 рядов, а не 5:
+ ```
+ x
+ xxx
+ xxxxx
+ xxxxxxx
+ xxxxxxxxx
+ ```*/
+
+let x = 'x';
+console.log(x);
+
+for (let i = 0; i < 20; i++) {
+    x = x + 'xx';
+    console.log(x);
+}
